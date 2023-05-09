5eeedf0677b4d705b0e8f37ebc846a389baa9da1
msclal <mika1shanela@gmail.com>
2023-05-09 03:07:17 -0700

    setup tailwind

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// import { Inter } from "@next/font/google";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+  );
+}
