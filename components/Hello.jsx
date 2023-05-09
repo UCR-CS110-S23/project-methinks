0ec6005b542e35d09f489009c4934799476bfe0b
msclal <mika1shanela@gmail.com>
2023-05-09 12:34:52 -0700

    test commit to check if github working again

--- components/Hello.jsx
+++ components/Hello.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Hello = () => {
-  return <div className="text-red-200 underline">Hello wolr</div>;
+  return <div className="text-red-200 underline">Hello world</div>;
 };
 
 export default Hello;
