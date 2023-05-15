3fdae4cd6ed9db05066e054443eaaaa09c5db9ce
msclal <mika1shanela@gmail.com>
2023-05-15 16:21:29 -0700

    handle case when there are no comments

--- components/Feed/Comments.jsx
+++ components/Feed/Comments.jsx
@@ -39,12 +39,12 @@ const Comments = () => {
       date: "08:30:05 PM",
       text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor",
     },
-    {
-      img: "Profile",
-      username: "@blah",
-      date: "08:30:05 PM",
-      text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor",
-    },
+    // {
+    //   img: "Profile",
+    //   username: "@blah",
+    //   date: "08:30:05 PM",
+    //   text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor",
+    // },
     // {
     //   img: "Profile",
     //   username: "@blah",
@@ -58,35 +58,32 @@ const Comments = () => {
     //   text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor",
     // },
   ];
-  // WHAT IF NO COMMENTS?
   return (
     <div className="h-full w-1/2 bg-[#1C1C1C] p-5">
-      <div className="flex w-full h-full flex-col justify-between gap-y-10">
-        <div
-          className={`${
-            commentData.length > 6 ? `h-[51.5rem]` : ``
-          } overflow-y-auto scrollbar-none`}
-        >
-          {/* <div
-          className={`${
-            commentData.length > 3 ? `h-[52rem]` : ``
-          } overflow-y-auto scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-rparts-bannerGray`}
-        > */}
-          <p className="mb-5 font-semibold text-2xl text-white">Comments</p>
-          {/* <span className="">X</span> */}
-          {/* </div> */}
-          <div className="gap-y-5 flex flex-col">
-            {commentData.map((comment, index) => (
-              <Comment
-                key={index}
-                img={comment.img}
-                username={comment.username}
-                date={comment.date}
-                text={comment.text}
-              />
-            ))}
+      <div className="flex w-full h-full flex-col gap-y-8">
+        <p className="font-semibold text-2xl text-white">Comments</p>
+        {commentData.length === 0 ? (
+          <div className="h-[47rem] flex justify-center items-center">
+            <div className="text-white text-center text-2xl flex flex-col font-semibold">
+              <p>The minds are calm and quiet...</p>
+              <p>Be the first to build on this thought!</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="h-[47rem] overflow-y-auto scrollbar-none">
+            <div className="gap-y-5 flex flex-col">
+              {commentData.map((comment, index) => (
+                <Comment
+                  key={index}
+                  img={comment.img}
+                  username={comment.username}
+                  date={comment.date}
+                  text={comment.text}
+                />
+              ))}
+            </div>
+          </div>
+        )}
         <div className="bg-white rounded-3xl text-black flex items-center w-full px-4 py-2">
           <input
             name="comment"
@@ -94,7 +91,7 @@ const Comments = () => {
             placeholder="Expand this thought..."
             className="w-full bg-white focus:outline-none"
           />
-          <p className="">{`>`}</p>
+          <p className="cursor-pointer">{`>`}</p>
         </div>
       </div>
     </div>
