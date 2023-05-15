151751ba99b5d492982113b4698a979ee6d3c69f
msclal <mika1shanela@gmail.com>
2023-05-15 11:33:17 -0700

    change file casing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // import { Inter } from "@next/font/google";
 
-import Feed from "./Feed";
+import Feed from "./feed";
 
 // eslint-disable-next-line new-cap
 // const inter = Inter({ subsets: ["latin"] });
