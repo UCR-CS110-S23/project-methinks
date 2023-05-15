963ca34e1462c46ffc46b4efd8aa7cfbe19c36dc
Mariam Golwalla <mgolw001@ucr.edu>
2023-05-15 11:23:30 -0700

    public sans added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import Layout from "@/components/Layout";
 import "../styles/globals.css";
+/* eslint-disable camelcase */
+import { Public_Sans } from "@next/font/google";
+
+/* eslint-disable new-cap */
+const publicSans = Public_Sans({
+  subsets: ["latin"],
+  variable: "--font-publicSans",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={`${publicSans.className}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   );
 }
