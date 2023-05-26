039a77203c0a8784b79bc1b795accd144126b858
Kliang622 <kliang622@gmail.com>
2023-05-25 22:41:21 -0700

    nav bar finally done

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Cards from "@/components/Post/Cards";
 import Tags from "@/components/Post/Tags";
+import Navigation from "./Navigation";
 
 const Post = () => {
   const [text, setText] = useState("");
@@ -43,6 +44,7 @@ const Post = () => {
       >
         Post
       </button>
+      <Navigation />
     </div>
   );
 };
