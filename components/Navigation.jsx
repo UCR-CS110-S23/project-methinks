d5af74128aa41d9b2e2f65a02be471fdc26744ad
Kliang622 <kliang622@gmail.com>
2023-05-29 17:45:42 -0700

    navBar dropdown bones

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavDropdown from "./NavDropdown";
 
 const Navigation = () => {
   return (
@@ -16,6 +17,7 @@ const Navigation = () => {
           />
         </div>
       </nav>
+      <NavDropdown/>
     </div>
   );
 };
