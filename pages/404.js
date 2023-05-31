acfb48612ed5dd4e4b15154c65e3677bbf24ffa9
ashley-kim22 <ashkim316@gmail.com>
2023-05-31 00:18:18 -0700

    404 page text

--- pages/404.js
+++ pages/404.js
@@ -6,10 +6,10 @@ export default function Error404() {
     <div className="bg-methinks-background w-full h-screen flex flex-col justify-center items-center text-white">
       <title>404</title>
       <div className="absolute justify-center items-center -translate-y-[10%]">
-        <Image src={GreyCloud} alt="cloud" />
+        <Image src={GreyCloud} alt="cloud" draggable={false} />
       </div>
 
-      <div className="flex flex-col items-center justify-center h-fit w-[480px] h-[480px]">
+      <div className="flex flex-col items-center justify-center h-fit w-[480px] h-[480px] z-10 absolute">
         <p className=" text-9xl m-0 mt-2">404</p>
         <p className=" text-lg mt-4">Page not found!</p>
       </div>
