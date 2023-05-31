88b17223f6655d9ebad22714a07e9d8ba729a085
ashley-kim22 <ashkim316@gmail.com>
2023-05-31 00:16:47 -0700

    404 page more cloud format

--- pages/404.js
+++ pages/404.js
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Error404() {
   return (
-    <div className="flex items-center justify-center text-white">
+    <div className="bg-methinks-background w-full h-screen flex flex-col justify-center items-center text-white">
       <title>404</title>
-      <div className="absolute top-0 left-0 overflow-hidden z-0 bg-beatdrop-lightgrey ">
-        <Image src={GreyCloud} alt="cloud" draggable={false} />
+      <div className="absolute justify-center items-center -translate-y-[10%]">
+        <Image src={GreyCloud} alt="cloud" />
       </div>
 
-      <div className="flex flex-col items-center justify-center w-[480px] h-[480px]  z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-0 p-0">
-        <p className=" text-9xl m-0 mt-2 ">404</p>
-        <p className=" text-mg -mt-3">Page not found!</p>
+      <div className="flex flex-col items-center justify-center h-fit w-[480px] h-[480px]">
+        <p className=" text-9xl m-0 mt-2">404</p>
+        <p className=" text-lg mt-4">Page not found!</p>
       </div>
     </div>
   );
