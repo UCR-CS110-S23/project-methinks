b974b6d814f1b9affcfb06444d20ebb442ad3f05
Mariam Golwalla <mgolw001@ucr.edu>
2023-05-31 00:05:35 -0700

    comment page has tweet and comments

--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -1,11 +1,21 @@
 // add the post with comments here
 import React from "react";
-
+import Post from "@/components/Feed/Post";
 import Comments from "@/components/Feed/Comments";
 
 const CommentPage = () => {
   return (
     <div className="flex flex-col justify-center items-center ">
+      <div className="w-1/2 m-2">
+        <Post
+          id={1}
+          pic="/henry2.jpg"
+          name="mariam"
+          time="1:11"
+          post="i love you"
+          tag="sports"
+        />
+      </div>
       <Comments />
     </div>
   );
