039545511629476c1c7a692dd0b09ed896c63ec0
msclal <mika1shanela@gmail.com>
2023-05-31 23:49:02 -0700

    refactored implementation for dynamically routed pages

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useRouter } from "next/router";
 // import UserProfile from "@/components/Profile/UserProfile";
 import FriendsProfile from "@/components/Profile/FriendsProfile";
 
-import { getAllUserIds, getUserData } from "@/lib/users";
+import { getUserData } from "@/lib/users";
 
 export default function Profile({ userData }) {
   const user = JSON.parse(userData)[0];
+  const router = useRouter();
+
+  if (!user) {
+    router.replace("/404");
+    return;
+  }
   return (
     user && (
       <>
@@ -15,18 +22,28 @@ export default function Profile({ userData }) {
   );
 }
 
-// check for valid dynamic routes
-export async function getStaticPaths() {
-  const paths = await getAllUserIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
+// // check for valid dynamic routes
+// export async function getStaticPaths() {
+//   const paths = await getAllUserIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-// gets current route id's data
-// localhost:3000/post/[id]
-export async function getStaticProps({ params }) {
+// // gets current route id's data
+// // localhost:3000/post/[id]
+// export async function getStaticProps({ params }) {
+//   const userData = await getUserData(params.id);
+//   return {
+//     props: {
+//       userData,
+//     },
+//   };
+// }
+
+export async function getServerSideProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
   const userData = await getUserData(params.id);
   return {
     props: {
