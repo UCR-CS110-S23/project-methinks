e9a787eea137490bea8a8c1d3aacc2520a7066b0
ashley-kim22 <ashkim316@gmail.com>
2023-06-06 21:15:51 -0700

    think out loud icon

--- components/Post/ThinkOutLoud.jsx
+++ components/Post/ThinkOutLoud.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GiSoundWaves } from "react-icons/gi";
 
 const Fleeting = () => {
   return (
@@ -8,10 +9,13 @@ const Fleeting = () => {
       </p>
       <textarea
         name="thought"
-        className="focus:outline-none resize-none w-full h-2/3 px-6 text-3xl font-normal text-methinks-white bg-methinks-lightblack text-whhite"
+        className="focus:outline-none resize-none w-full h-1/3 px-6 text-3xl font-normal text-methinks-white bg-methinks-lightblack text-whhite "
         placeholder="Speak now or forever hold your peace!"
         disabled
       />
+      <div className="flex justify-center">
+        <GiSoundWaves className=" text-9xl  " />
+      </div>
     </div>
   );
 };
